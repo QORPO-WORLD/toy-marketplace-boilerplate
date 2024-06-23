cc59af62bd4089888d41bac510e9780c9c68a58d
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-06-23 14:57:21 +0200

    prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,19 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: [
+    "prettier-plugin-tailwindcss",
+    "@trivago/prettier-plugin-sort-imports",
+  ],
+  singleQuote: true,
+  trailingComma: "all",
+  importOrder: [
+    "^react(.*)",
+    "<THIRD_PARTY_MODULES>",
+    "~/(.*)",
+    "$.*",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
 };
 
 export default config;
