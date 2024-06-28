2cb5ddf61ad58c408ffac17dfc98b1aad1d21f91
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-06-28 13:47:29 +0200

    order cart button

--- src/app/_layout/Header/Buttons/OrderCartButton.tsx
+++ src/app/_layout/Header/Buttons/OrderCartButton.tsx
@@ -1,8 +1,11 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import { cartState, toggleCart } from '~/lib/stores/cart/Cart';
 
 import { Button, Box, cn, CartIcon, Text } from '$ui';
+import { useSnapshot } from 'valtio';
 
 export const OrderCartButton = () => {
   return (
@@ -10,7 +13,7 @@ export const OrderCartButton = () => {
       className="relative backdrop-blur"
       variant="muted"
       aria-label="Cart"
-      // onClick={() => toggleCart()}
+      onClick={() => toggleCart()}
     >
       <CartIcon />
       <CartItemsBadge />
@@ -18,17 +21,17 @@ export const OrderCartButton = () => {
   );
 };
 const CartItemsBadge = () => {
-  // const { cartItems } = useSnapshot(cartState);
+  const { cartItems } = useSnapshot(cartState);
   const [animate, setAnimate] = useState(false);
 
-  // useEffect(() => {
-  //   if (cartItems.length) {
-  //     setAnimate(true);
-  //     setTimeout(() => setAnimate(false), 500);
-  //   }
-  // }, [cartItems.length]);
+  useEffect(() => {
+    if (cartItems.length) {
+      setAnimate(true);
+      setTimeout(() => setAnimate(false), 500);
+    }
+  }, [cartItems.length]);
 
-  // if (!cartItems.length) return null;
+  if (!cartItems.length) return null;
 
   return (
     <Box
@@ -36,11 +39,11 @@ const CartItemsBadge = () => {
         'absolute right-1 top-1',
         'bg-primary rounded-full',
         animate ? 'animate-ping' : '',
-        // cartItems.length < 10 ? 'px-[8px]' : 'px-[5px]',
+        cartItems.length < 10 ? 'px-[8px]' : 'px-[5px]',
       )}
     >
       <Text className="text-primary-foreground text-[10px]">
-        {/* {cartItems.length} */}
+        {cartItems.length}
       </Text>
     </Box>
   );
