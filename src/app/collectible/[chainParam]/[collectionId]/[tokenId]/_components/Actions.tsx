fc5353a1360aaf4580029d7e950783671c9cfa51
Mithat Akbulut <marmarisco860@gmail.com>
2024-10-23 14:08:24 +0300

    trigger modals from the sdk when clicking on offer and list ctas

--- src/app/collectible/[chainParam]/[collectionId]/[tokenId]/_components/Actions.tsx
+++ src/app/collectible/[chainParam]/[collectionId]/[tokenId]/_components/Actions.tsx
@@ -8,9 +8,11 @@ import { Button, Dialog, Flex, ScrollArea, Text } from '$ui';
 import { useCollectableData } from '../_hooks/useCollectableData';
 import { MarketplaceKind } from '@0xsequence/marketplace-sdk';
 import {
+  useCreateListingModal,
   useCurrencies,
   useHighestOffer,
   useLowestListing,
+  useMakeOfferModal,
   useTokenBalances,
 } from '@0xsequence/marketplace-sdk/react';
 import { useAccount } from 'wagmi';
@@ -25,8 +27,9 @@ export const CollectibleTradeActions = ({
   tokenId,
   collectionAddress,
 }: CollectibleTradeActionsProps) => {
-  const [isOfferModalOpen, setIsOfferModalOpen] = useState(false);
-  const [isListingModalOpen, setIsListingModalOpen] = useState(false);
+  const { show: showListModal, close: closeListModal } =
+    useCreateListingModal();
+  const { show: showOfferModal, close: closeOfferModal } = useMakeOfferModal();
 
   const { data: currencies } = useCurrencies({
     chainId,
@@ -98,6 +101,22 @@ export const CollectibleTradeActions = ({
     //todo sell
   };
 
+  const onClickOffer = () => {
+    showOfferModal({
+      collectionAddress,
+      chainId: String(chainId),
+      collectibleId: tokenId,
+    });
+  };
+
+  const onClickList = () => {
+    showListModal({
+      collectionAddress,
+      chainId: String(chainId),
+      collectibleId: tokenId,
+    });
+  };
+
   const buyDisabled = !bestListings || item721AlreadyOwned;
   const offerDisabled = !isConnected;
   const listingDisabled = !isConnected || !userBalance;
@@ -116,38 +135,15 @@ export const CollectibleTradeActions = ({
           <Text className="text-inherit">Buy</Text>
         </Button>
         <Flex className="w-full flex-col gap-3">
-          <Dialog.Root
-            open={isOfferModalOpen}
-            onOpenChange={(isOpen) => setIsOfferModalOpen(isOpen)}
+          <Button
+            className="w-full justify-between"
+            onClick={onClickOffer}
+            size="lg"
+            loading={false}
+            disabled={offerDisabled}
           >
-            <Dialog.Trigger asChild>
-              <Button
-                className="w-full justify-between"
-                size="lg"
-                loading={false}
-                disabled={offerDisabled}
-              >
-                <Text className="text-inherit">Offer</Text>
-              </Button>
-            </Dialog.Trigger>
-
-            <Dialog.BaseContent
-              container={getThemeManagerElement()}
-              className="max-h-screen max-w-[700px] p-5"
-              onOpenAutoFocus={(e) => e.preventDefault()}
-            >
-              <Dialog.Title>Create an offer</Dialog.Title>
-              {/* <OrderModalContent */}
-              {/*   chainId={chainId} */}
-              {/*   collectionAddress={collectionAddress} */}
-              {/*   tokenId={tokenId} */}
-              {/*   bestOrder={bestListing} */}
-              {/*   open={isOfferModalOpen} */}
-              {/*   setOpen={setIsOfferModalOpen} */}
-              {/*   type="offer" */}
-              {/* /> */}
-            </Dialog.BaseContent>
-          </Dialog.Root>
+            <Text className="text-inherit">Offer</Text>
+          </Button>
         </Flex>
       </Flex>
       <Flex className="flex-row gap-4">
@@ -162,43 +158,15 @@ export const CollectibleTradeActions = ({
         </Button>
 
         <Flex className="w-full flex-col gap-3">
-          <Dialog.Root
-            open={isListingModalOpen}
-            onOpenChange={(isOpen) => setIsListingModalOpen(isOpen)}
+          <Button
+            className="w-full justify-between"
+            onClick={onClickList}
+            size="lg"
+            loading={false}
+            disabled={listingDisabled}
           >
-            <Dialog.Trigger asChild>
-              <Button
-                className="w-full justify-between"
-                size="lg"
-                loading={false}
-                disabled={listingDisabled}
-              >
-                <Text className="text-inherit">List</Text>
-              </Button>
-            </Dialog.Trigger>
-
-            <Dialog.BaseContent
-              container={getThemeManagerElement()}
-              className="flex max-w-[700px] flex-col overflow-hidden p-0"
-              onOpenAutoFocus={(e) => e.preventDefault()}
-            >
-              <ScrollArea.Base viewportClassName="max-h-screen">
-                <Flex className="h-full w-full flex-col gap-4 p-5">
-                  <Dialog.Title>Make a listing</Dialog.Title>
-
-                  {/* <OrderModalContent */}
-                  {/*   chainId={chainId} */}
-                  {/*   collectionAddress={collectionAddress} */}
-                  {/*   tokenId={tokenId} */}
-                  {/*   bestOrder={bestOffer} */}
-                  {/*   open={isListingModalOpen} */}
-                  {/*   setOpen={setIsListingModalOpen} */}
-                  {/*   type="listing" */}
-                  {/* /> */}
-                </Flex>
-              </ScrollArea.Base>
-            </Dialog.BaseContent>
-          </Dialog.Root>
+            <Text className="text-inherit">List</Text>
+          </Button>
         </Flex>
       </Flex>
     </Flex>
