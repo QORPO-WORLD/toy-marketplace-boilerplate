78bb849173950c8c2e577b633187b9d75af19d57
Mithat Akbulut <marmarisco860@gmail.com>
2024-11-06 18:17:57 +0300

    use hooks for showing modals

--- src/components/buttons/AddToCartButton.tsx
+++ src/components/buttons/AddToCartButton.tsx
@@ -1,46 +1,101 @@
 'use client';
 
 import { Button } from '$ui';
-
-const orderTypes = {
-  buy: {
-    label: 'Buy',
-    onClick: () => {},
-  },
-  sell: {
-    label: 'Sell',
-    onClick: () => {},
-  },
-  transfer: {
-    label: 'Transfer',
-    onClick: () => {},
-  },
-  order: {
-    label: 'Place offer',
-    onClick: () => {},
-  },
-  listing: {
-    label: 'Create Listing',
-    onClick: () => {},
-  },
-};
+import { Order } from '@0xsequence/marketplace-sdk';
+import {
+  useCreateListingModal,
+  useMakeOfferModal,
+  useSellModal,
+  useTransferModal,
+} from '@0xsequence/marketplace-sdk/react';
+import { Hex } from 'viem';
 
 type OrderSide = 'buy' | 'sell' | 'transfer' | 'order' | 'listing';
 
 type AddToCartButtonProps = {
   className?: string;
   orderSide: OrderSide;
+  tokenId: string;
+  collectionAddress: string;
+  chainId: string;
+  receivedOffer?: Order;
+  collectibleName?: string;
 };
 
 export const AddToCartButton = ({
   className,
   orderSide,
+  tokenId,
+  collectionAddress,
+  chainId,
+  receivedOffer,
+  collectibleName,
 }: AddToCartButtonProps) => {
-  const onClick = () => {};
+  const { show: showCreateListingModal } = useCreateListingModal();
+  const { show: showMakeOfferModal } = useMakeOfferModal();
+  const { show: showSellModal } = useSellModal();
+  const { show: showTransferModal } = useTransferModal();
+
+  const orderTypes = {
+    buy: {
+      label: 'Buy',
+      // TODO: Implement buy
+      onClick: () => {
+        console.log('buy');
+      },
+    },
+    sell: {
+      label: 'Sell',
+      onClick: () => {
+        if (!receivedOffer || !collectibleName)
+          throw new Error(
+            'receivedOffer and collectibleName are required for sell',
+          );
+
+        showSellModal({
+          tokenId,
+          collectionAddress,
+          chainId,
+          order: receivedOffer!,
+          collectibleName: collectibleName!,
+        });
+      },
+    },
+    transfer: {
+      label: 'Transfer',
+      onClick: () => {
+        showTransferModal({
+          tokenId,
+          collectionAddress: collectionAddress as Hex,
+          chainId,
+        });
+      },
+    },
+    order: {
+      label: 'Place offer',
+      onClick: () => {
+        showMakeOfferModal({
+          collectionAddress,
+          chainId,
+          collectibleId: tokenId,
+        });
+      },
+    },
+    listing: {
+      label: 'Create Listing',
+      onClick: () => {
+        showCreateListingModal({
+          collectionAddress: '0x0',
+          chainId: '1',
+          collectibleId: '0',
+        });
+      },
+    },
+  };
   const { label } = orderTypes[orderSide];
 
   return (
-    <Button onClick={onClick} className={className}>
+    <Button onClick={orderTypes[orderSide].onClick} className={className}>
       {label}
     </Button>
   );
