6852a3e9ef1f516d8a2753c3be5474d4dc9e19e6
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-11-15 08:57:05 +0100

    Fix sdk config

--- src/config/marketplace-sdk/config.ts
+++ src/config/marketplace-sdk/config.ts
@@ -1,8 +1,21 @@
-import { env } from "~/env";
+import { env } from '~/env';
+
+import { type SdkConfig } from '@0xsequence/marketplace-sdk';
+
+const embeddedWallet = env.NEXT_PUBLIC_WAAS_CONFIG_KEY
+  ? ({
+      waasConfigKey: env.NEXT_PUBLIC_WAAS_CONFIG_KEY,
+      googleClientId: env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      appleClientId: env.NEXT_PUBLIC_APPLE_CLIENT_ID,
+      appleRedirectURI: env.NEXT_PUBLIC_APPLE_REDIRECT_URI,
+    } satisfies NonNullable<SdkConfig['wallet']>['embedded'])
+  : undefined;
 
 export const config = {
-  projectAccessKey: 'AQAAAAAAADVH8R2AGuQhwQ1y8NaEf1T7PJM', //env.NEXT_PUBLIC_SEQUENCE_ACCESS_KEY,
+  projectId: env.NEXT_PUBLIC_SEQUENCE_PROJECT_ID,
+  projectAccessKey: env.NEXT_PUBLIC_SEQUENCE_ACCESS_KEY,
   wallet: {
     walletConnectProjectId: env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+    embedded: embeddedWallet,
   },
-};
+} satisfies SdkConfig;
