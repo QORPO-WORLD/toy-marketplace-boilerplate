8a68dca0396c1faafae9683fe06207620d17fc65
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-11-18 10:13:16 +0100

    Format

--- src/app/collectible/[chainParam]/[collectionId]/[tokenId]/_components/Actions.tsx
+++ src/app/collectible/[chainParam]/[collectionId]/[tokenId]/_components/Actions.tsx
@@ -1,196 +1,188 @@
 'use client';
 
-import { useState } from 'react';
-
-import { getThemeManagerElement } from '~/lib/utils/theme';
-
-import { Button, Dialog, Flex, ScrollArea, Text, toast } from '$ui';
-import { useCollectableData } from '../_hooks/useCollectableData';
+import { Button, Flex, Text, toast } from '$ui';
 import { MarketplaceKind } from '@0xsequence/marketplace-sdk';
 import {
-  useCreateListingModal,
-  useCurrencies,
-  useHighestOffer,
-  useLowestListing,
-  useMakeOfferModal,
-  useSellModal,
-  useBalanceOfCollectible,
+	useBalanceOfCollectible,
+	useCreateListingModal,
+	useCurrencies,
+	useHighestOffer,
+	useLowestListing,
+	useMakeOfferModal,
+	useSellModal,
 } from '@0xsequence/marketplace-sdk/react';
-import { useAccount, useConnectorClient, useConnectors } from 'wagmi';
-import { type Hex } from 'viem';
+import type { Hex } from 'viem';
+import { useAccount } from 'wagmi';
+import { useCollectableData } from '../_hooks/useCollectableData';
 
 interface CollectibleTradeActionsProps {
-  chainId: number;
-  tokenId: string;
-  collectionAddress: Hex;
+	chainId: number;
+	tokenId: string;
+	collectionAddress: Hex;
 }
 export const CollectibleTradeActions = ({
-  chainId,
-  tokenId,
-  collectionAddress,
+	chainId,
+	tokenId,
+	collectionAddress,
 }: CollectibleTradeActionsProps) => {
-  const { show: showListModal } = useCreateListingModal();
-  const { show: showOfferModal } = useMakeOfferModal();
-  const { show: showSellModal } = useSellModal();
+	const { onError: onListingError, show: showListModal } = useCreateListingModal();
+	const { show: showOfferModal } = useMakeOfferModal();
+	const { show: showSellModal } = useSellModal();
 
-  const { data: currencies } = useCurrencies({
-    chainId,
-    collectionAddress,
-  });
+	const { data: currencies } = useCurrencies({
+		chainId,
+		collectionAddress,
+	});
 
-  const currencyAddresses = currencies?.map((c) => c.contractAddress) || [];
+	
 
-  const { data: highestOffer, isLoading: isLoadingHighestOffer } =
-    useHighestOffer({
-      chainId: String(chainId),
-      collectionAddress,
-      tokenId: tokenId,
-      filter: {
-        marketplace: [MarketplaceKind.sequence_marketplace_v1],
-        currencies: currencyAddresses,
-      },
-      query: {
-        enabled: !!currencies,
-      },
-    });
+	const currencyAddresses = currencies?.map((c) => c.contractAddress) || [];
 
-  const { data: lowestListing, isLoading: loadingLowestListing } =
-    useLowestListing({
-      chainId: String(chainId),
-      collectionAddress,
-      tokenId,
-      filters: {
-        marketplace: [MarketplaceKind.sequence_marketplace_v1],
-      },
-      query: {
-        enabled: !!currencies,
-      },
-    });
+	const { data: highestOffer, isLoading: isLoadingHighestOffer } =
+		useHighestOffer({
+			chainId: String(chainId),
+			collectionAddress,
+			tokenId: tokenId,
+			filter: {
+				marketplace: [MarketplaceKind.sequence_marketplace_v1],
+				currencies: currencyAddresses,
+			},
+			query: {
+				enabled: !!currencies,
+			},
+		});
 
-  const { collectionMetadata, collectibleMetadata } = useCollectableData();
+	const { data: lowestListing, isLoading: loadingLowestListing } =
+		useLowestListing({
+			chainId: String(chainId),
+			collectionAddress,
+			tokenId,
+			filters: {
+				marketplace: [MarketplaceKind.sequence_marketplace_v1],
+			},
+			query: {
+				enabled: !!currencies,
+			},
+		});
 
-  const isERC1155 = collectionMetadata.data?.type === 'ERC1155';
+	const { collectionMetadata, collectibleMetadata } = useCollectableData();
 
-  const { address, isConnected } = useAccount();
+	const isERC1155 = collectionMetadata.data?.type === 'ERC1155';
 
-  const { data: userBalanceResp, isLoading: isBalanceLoading } =
-    useBalanceOfCollectible({
-      chainId: chainId,
-      collectionAddress,
-      collectableId: tokenId,
-      query: {
-        enabled: !!isConnected && !!address,
-      },
-    });
+	const { address, isConnected } = useAccount();
 
-  const tokenBalance = userBalanceResp?.balance;
+	const { data: userBalanceResp, isLoading: isBalanceLoading } =
+		useBalanceOfCollectible({
+			chainId: chainId,
+			collectionAddress,
+			collectableId: tokenId,
+			userAddress: address,
+			query: {
+				enabled: !!isConnected && !!address,
+			},
+		});
 
-  const item721AlreadyOwned = !!tokenBalance && !isERC1155;
+	const tokenBalance = userBalanceResp?.balance;
 
-  const isLoading =
-    isLoadingHighestOffer ||
-    loadingLowestListing ||
-    (isConnected && isBalanceLoading);
+	const item721AlreadyOwned = !!tokenBalance && !isERC1155;
 
-  const onClickBuy = () => {
-    //TODO: buy
-  };
+	const isLoading =
+		isLoadingHighestOffer ||
+		loadingLowestListing ||
+		(isConnected && isBalanceLoading);
 
-  const onClickSell = () => {
-    showSellModal({
-      collectionAddress,
-      chainId: String(chainId),
-      tokenId,
-      collectibleName: collectibleMetadata.data?.name,
-      order: highestOffer!.order!,
-      messages: {
-        sellCollectible: {
-          onUnknownError: (error: any) => {
-            console.log(error);
-          },
-        },
-      },
-    });
-  };
+	const onClickBuy = () => {
+		//TODO: buy
+	};
 
-  const onClickOffer = () => {
-    showOfferModal({
-      collectionAddress,
-      chainId: String(chainId),
-      collectibleId: tokenId,
-    });
-  };
+	const onClickSell = () => {
+		showSellModal({
+			collectionAddress,
+			chainId: String(chainId),
+			tokenId,
+			collectibleName: collectibleMetadata.data?.name,
+			order: highestOffer!.order!,
+		});
+	};
 
-  const onClickList = () => {
-    showListModal({
-      collectionAddress,
-      chainId: String(chainId),
-      collectibleId: tokenId,
-      messages: {
-        createListing: {
-          onSuccess: () => {
-            toast.success('Listing created successfully');
-          },
-          onUnknownError: (error: any) => {
-            toast.warning('An error occurred while creating the listing');
-          },
-        },
-      },
-    });
-  };
+	const onClickOffer = () => {
+		showOfferModal({
+			collectionAddress,
+			chainId: String(chainId),
+			collectibleId: tokenId,
+		});
+	};
 
-  const buyDisabled = !isConnected || !lowestListing || item721AlreadyOwned;
-  const offerDisabled = !isConnected || item721AlreadyOwned;
-  const listingDisabled = !isConnected || !tokenBalance;
-  const sellDisabled = !isConnected || !highestOffer?.order || !tokenBalance;
+	const onClickList = () => {
+		showListModal({
+			collectionAddress,
+			chainId: String(chainId),
+			collectibleId: tokenId,
+			messages: {
+				createListing: {
+					onSuccess: () => {
+						toast.success('Listing created successfully');
+					},
+					onUnknownError: (error: any) => {
+						toast.warning('An error occurred while creating the listing');
+					},
+				},
+			},
+		});
+	};
 
-  return (
-    <Flex className="flex-col gap-4">
-      <Flex className="flex-row gap-4">
-        <Button
-          size="lg"
-          className="w-full justify-between"
-          loading={isLoading}
-          disabled={buyDisabled}
-          onClick={onClickBuy}
-        >
-          <Text className="text-inherit">Buy</Text>
-        </Button>
-        <Flex className="w-full flex-col gap-3">
-          <Button
-            className="w-full justify-between"
-            onClick={onClickOffer}
-            size="lg"
-            loading={false}
-            disabled={offerDisabled}
-          >
-            <Text className="text-inherit">Offer</Text>
-          </Button>
-        </Flex>
-      </Flex>
-      <Flex className="flex-row gap-4">
-        <Button
-          className="w-full justify-between"
-          size="lg"
-          loading={isLoading}
-          disabled={sellDisabled}
-          onClick={onClickSell}
-        >
-          <Text className="text-inherit">Sell</Text>
-        </Button>
+	const buyDisabled = !isConnected || !lowestListing || item721AlreadyOwned;
+	const offerDisabled = !isConnected || item721AlreadyOwned;
+	const listingDisabled = !isConnected || !tokenBalance;
+	const sellDisabled = !isConnected || !highestOffer?.order || !tokenBalance;
 
-        <Flex className="w-full flex-col gap-3">
-          <Button
-            className="w-full justify-between"
-            onClick={onClickList}
-            size="lg"
-            loading={false}
-            disabled={listingDisabled}
-          >
-            <Text className="text-inherit">List</Text>
-          </Button>
-        </Flex>
-      </Flex>
-    </Flex>
-  );
+	return (
+		<Flex className="flex-col gap-4">
+			<Flex className="flex-row gap-4">
+				<Button
+					size="lg"
+					className="w-full justify-between"
+					loading={isLoading}
+					disabled={buyDisabled}
+					onClick={onClickBuy}
+				>
+					<Text className="text-inherit">Buy</Text>
+				</Button>
+				<Flex className="w-full flex-col gap-3">
+					<Button
+						className="w-full justify-between"
+						onClick={onClickOffer}
+						size="lg"
+						loading={false}
+						disabled={offerDisabled}
+					>
+						<Text className="text-inherit">Offer</Text>
+					</Button>
+				</Flex>
+			</Flex>
+			<Flex className="flex-row gap-4">
+				<Button
+					className="w-full justify-between"
+					size="lg"
+					loading={isLoading}
+					disabled={sellDisabled}
+					onClick={onClickSell}
+				>
+					<Text className="text-inherit">Sell</Text>
+				</Button>
+
+				<Flex className="w-full flex-col gap-3">
+					<Button
+						className="w-full justify-between"
+						onClick={onClickList}
+						size="lg"
+						loading={false}
+						disabled={listingDisabled}
+					>
+						<Text className="text-inherit">List</Text>
+					</Button>
+				</Flex>
+			</Flex>
+		</Flex>
+	);
 };
