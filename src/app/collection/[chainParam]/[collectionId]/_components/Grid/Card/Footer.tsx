bebbc3ad51337e763f53e1ecc50aaae76724d0a9
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-11-18 13:38:20 +0100

    Fix imports

--- src/app/collection/[chainParam]/[collectionId]/_components/Grid/Card/Footer.tsx
+++ src/app/collection/[chainParam]/[collectionId]/_components/Grid/Card/Footer.tsx
@@ -1,75 +1,78 @@
 import { Avatar, Badge, Flex, Text, cn } from '~/components/ui';
 
-import { type TokenMetadata } from '@0xsequence/indexer';
-import { type Order, truncateMiddle } from '@0xsequence/marketplace-sdk';
+import type { TokenMetadata } from '@0xsequence/indexer';
+import {
+	type Order as OrderType,
+	truncateMiddle,
+} from '@0xsequence/marketplace-sdk';
 import { useCurrencies } from '@0xsequence/marketplace-sdk/react';
 
 type FooterProps = {
-  tokenMetadata: TokenMetadata;
-  order?: Order;
+	tokenMetadata: TokenMetadata;
+	order?: OrderType;
 };
 
 export const Footer = ({ tokenMetadata, order }: FooterProps) => {
-  const { tokenId, name } = tokenMetadata;
+	const { tokenId, name } = tokenMetadata;
 
-  const height = 'h-[24px]';
-  return (
-    <>
-      <Text
-        className={cn(
-          height,
-          'md:text-md text-left text-xs font-medium text-foreground/50 max-lines-[1]',
-        )}
-      >
-        #{truncateMiddle(tokenId, 10) || '--'}
-      </Text>
+	const height = 'h-[24px]';
+	return (
+		<>
+			<Text
+				className={cn(
+					height,
+					'md:text-md text-left text-xs font-medium text-foreground/50 max-lines-[1]',
+				)}
+			>
+				#{truncateMiddle(tokenId, 10) || '--'}
+			</Text>
 
-      <Text
-        className={cn(
-          height,
-          'md:text-md ellipsis block text-left font-semibold text-foreground',
-        )}
-        title={name}
-      >
-        {name || '<unknown>'}
-      </Text>
+			<Text
+				className={cn(
+					height,
+					'md:text-md ellipsis block text-left font-semibold text-foreground',
+				)}
+				title={name}
+			>
+				{name || '<unknown>'}
+			</Text>
 
-      {order && <Order height={height} order={order} />}
-    </>
-  );
+			{order && <Order height={height} order={order} />}
+		</>
+	);
 };
 
 type OrderProps = {
-  height: string;
-  order: Order;
+	height: string;
+	order: OrderType;
 };
 
 const Order = ({ height, order }: OrderProps) => {
-  const { data: currencies } = useCurrencies({
-    chainId: order.chainId,
-  });
+	const { data: currencies } = useCurrencies({
+		chainId: order.chainId,
+	});
 
-  const currency = currencies?.find(
-    (c) => c.contractAddress === order.priceCurrencyAddress,
-  );
+	const currency = currencies?.find(
+		(c) => c.contractAddress === order.priceCurrencyAddress,
+	);
 
-  return (
-    <Flex className={cn(height, 'flex-1 items-center justify-between')}>
-      <Flex className="items-center gap-2">
-        <Avatar.Base size="xs">
-          <Avatar.Image src={currency?.imageUrl} />
-          <Avatar.Fallback>{currency?.name}</Avatar.Fallback>
-        </Avatar.Base>
-        <Text
-          className="ellipsis text-sm md:text-base"
-          title={String(currency?.name)}
-        >
-          {order.priceAmountFormatted || 'N/A'} {currency?.symbol}
-        </Text>
-      </Flex>
-      <Badge variant="success">
-        Stock: <span className="ml-1">{order.quantityRemainingFormatted}</span>
-      </Badge>
-    </Flex>
-  );
+	return (
+		<Flex className={cn(height, 'flex-1 items-center justify-between')}>
+			<Flex className="items-center gap-2">
+				<Avatar.Base size="xs">
+					<Avatar.Image src={currency?.imageUrl} />
+					<Avatar.Fallback>{currency?.name}</Avatar.Fallback>
+				</Avatar.Base>
+				<Text
+					className="ellipsis text-sm md:text-base"
+					title={String(currency?.name)}
+				>
+					{order.priceAmountFormatted || 'N/A'} {currency?.symbol}
+				</Text>
+			</Flex>
+			<Badge variant="success">
+				Stock: <span className="ml-1">{order.quantityRemainingFormatted}</span>
+			</Badge>
+		</Flex>
+	);
 };
