420825fe6c05306e2c313791004587c281593c1c
Mithat Akbulut <marmarisco860@gmail.com>
2024-11-19 15:45:07 +0300

    wrap config with try-catch

--- src/config/xmlhttp-config.js
+++ src/config/xmlhttp-config.js
@@ -1,11 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-if (typeof window === 'undefined') {
-  // @ts-expect-error types for xhr2 are not valid
-  import('xhr2')
-    .then((xhr2) => {
-      global.XMLHttpRequest = xhr2.default;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+try {
+  if (typeof window === 'undefined') {
+    // @ts-expect-error types for xhr2 are not valid
+    const xhr2 = require('xhr2');
+    global.XMLHttpRequest = xhr2.default;
+  }
+} catch (error) {
+  console.error('XMLHttpRequest configuration failed:', error);
 }
