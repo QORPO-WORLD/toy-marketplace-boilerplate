eba98216534c449288e15ff07caff3545ff021a4
Mithat Akbulut <marmarisco860@gmail.com>
2024-11-19 14:35:06 +0300

    update ssrClient to use async/await for headers retrieval

--- src/config/marketplace-sdk/ssr.ts
+++ src/config/marketplace-sdk/ssr.ts
@@ -2,8 +2,8 @@ import { config } from './config';
 import { createSSRClient } from '@0xsequence/marketplace-sdk/react/ssr';
 import { headers } from 'next/headers';
 
-export const ssrClient = () => {
-  const headersList = headers();
+export const ssrClient = async() => {
+  const headersList = await headers();
 
   return createSSRClient({
     cookie: headersList.get('cookie') || '',
