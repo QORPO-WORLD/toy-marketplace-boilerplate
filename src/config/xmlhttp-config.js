ded96f08cd54216e6e6e8a0c5204bafa90582312
Mithat Akbulut <marmarisco860@gmail.com>
2024-11-19 15:41:40 +0300

    fix server error

--- src/config/xmlhttp-config.js
+++ src/config/xmlhttp-config.js
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-
 if (typeof window === 'undefined') {
   // @ts-expect-error types for xhr2 are not valid
-  import('xhr2').then((xhr2) => {
-    global.XMLHttpRequest = xhr2.default;
-  }).catch((err) => {
-    console.error(err);
-  })
+  import('xhr2')
+    .then((xhr2) => {
+      global.XMLHttpRequest = xhr2.default;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
