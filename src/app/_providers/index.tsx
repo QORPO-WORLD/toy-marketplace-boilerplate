69ac461a74ee1353c2047230e787e9dc35830e80
Alexander Kolberg <alexander.kolberg@gmail.com>
2024-12-04 05:36:42 +0100

    add kit checkout provider

--- src/app/_providers/index.tsx
+++ src/app/_providers/index.tsx
@@ -2,7 +2,9 @@
 
 import { ToastProvider, Tooltip } from '$ui';
 import { ThemeProvider } from '@0xsequence/design-system';
+
 import { type KitConfig, KitProvider } from '@0xsequence/kit';
+import { KitCheckoutProvider } from "@0xsequence/kit-checkout";
 import {
   type MarketplaceConfig,
   type SdkConfig,
@@ -14,6 +16,7 @@ import {
   MarketplaceProvider,
   ModalProvider,
 } from '@0xsequence/marketplace-sdk/react';
+
 import { enableReactComponents } from '@legendapp/state/config/enableReactComponents';
 import { QueryClientProvider, useQuery } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -21,15 +24,17 @@ import { type State, WagmiProvider } from 'wagmi';
 
 const queryClient = getQueryClient();
 
+interface ProvidersProps {
+  sdkInitialState?: { wagmi?: State };
+  sdkConfig: SdkConfig;
+  children: React.ReactNode;
+}
+
 export default function Providers({
   sdkInitialState,
   sdkConfig,
   children,
-}: {
-  sdkInitialState?: { wagmi?: State };
-  sdkConfig: SdkConfig;
-  children: React.ReactNode;
-}) {
+}: ProvidersProps) {
   enableReactComponents();
 
   const { data: marketplaceConfig } = useQuery(
@@ -37,59 +42,41 @@ export default function Providers({
     queryClient,
   );
 
-  return marketplaceConfig ? (
-    <Providers2
-      config={sdkConfig}
-      marketplaceConfig={marketplaceConfig}
-      initialState={sdkInitialState}
-    >
-      {children}
-    </Providers2>
-  ) : (
-    <></>
-  );
-}
+  if (!marketplaceConfig) {
+    return null; //TODO
+  }
 
-const Providers2 = ({
-  config,
-  marketplaceConfig,
-  children,
-  initialState,
-}: {
-  config: SdkConfig;
-  marketplaceConfig: MarketplaceConfig;
-  children: React.ReactNode;
-  initialState?: { wagmi?: State };
-}) => {
-  const kitConfig = {
-    projectAccessKey: config.projectAccessKey,
+  const kitConfig: KitConfig = {
+    projectAccessKey: sdkConfig.projectAccessKey,
     signIn: {
       projectName: marketplaceConfig.title,
     },
-  } satisfies KitConfig;
+  };
 
   const wagmiConfig = createWagmiConfig(
     marketplaceConfig,
-    config,
-    !!initialState,
+    sdkConfig,
+    !!sdkInitialState,
   );
 
   return (
     <ThemeProvider>
-      <WagmiProvider config={wagmiConfig} initialState={initialState?.wagmi}>
+      <WagmiProvider config={wagmiConfig} initialState={sdkInitialState?.wagmi}>
         <QueryClientProvider client={queryClient}>
           <KitProvider config={kitConfig}>
-            <Tooltip.Provider>
-              <MarketplaceProvider config={config}>
-                {children}
-                <ReactQueryDevtools initialIsOpen={false} />
-                <ModalProvider />
-              </MarketplaceProvider>
-              <ToastProvider />
-            </Tooltip.Provider>
+            <KitCheckoutProvider>
+              <Tooltip.Provider>
+                <MarketplaceProvider config={sdkConfig}>
+                  {children}
+                  <ReactQueryDevtools initialIsOpen={false} />
+                  <ModalProvider />
+                </MarketplaceProvider>
+                <ToastProvider />
+              </Tooltip.Provider>
+            </KitCheckoutProvider>
           </KitProvider>
         </QueryClientProvider>
       </WagmiProvider>
     </ThemeProvider>
   );
-};
+}
