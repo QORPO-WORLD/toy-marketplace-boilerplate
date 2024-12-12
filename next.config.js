7424d1a39e194467d882d0325a1bcf6a92e42098
QORPO DEVOPS <devops@QORPOs-MacBook-Pro.local>
2024-12-12 15:57:45 +0100

    init

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import("next").NextConfig} */
 const config = {
   transpilePackages: ['@0xsequence/marketplace-sdk'],
+  basePath: '/market'
 };
 
 export default withVanillaExtract(config);
