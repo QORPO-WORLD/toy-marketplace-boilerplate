0e522c2907a93aed9399169850b5b628962d8110
Alexander Kolberg <alexander.kolberg@gmail.com>
2025-01-24 09:42:50 +0100

    add query client

--- src/config/marketplace-sdk/ssr.ts
+++ src/config/marketplace-sdk/ssr.ts
@@ -1,5 +1,6 @@
 import { config } from './config';
 import { createSSRClient } from '@0xsequence/marketplace-sdk/react/ssr';
+import { QueryClient } from '@tanstack/react-query';
 import { headers } from 'next/headers';
 
 export const ssrClient = () => {
@@ -7,6 +8,7 @@ export const ssrClient = () => {
 
   return createSSRClient({
     cookie: headersList.get('cookie') || '',
+    queryClient: new QueryClient(),
     config: {
       ...config,
     },
